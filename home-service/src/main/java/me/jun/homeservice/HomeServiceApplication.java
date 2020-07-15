c3c551254944d4e90f1a5b63eca136be6b98706b
kjh9267 <kjh9267z@naver.com>
2020-07-16 03:47:54 +0900

    Add Eureka Server

--- home-service/src/main/java/me/jun/homeservice/HomeServiceApplication.java
+++ home-service/src/main/java/me/jun/homeservice/HomeServiceApplication.java
@@ -4,9 +4,11 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableDiscoveryClient
 public class HomeServiceApplication {
 
     public static void main(String[] args) {
